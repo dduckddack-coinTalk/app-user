5d4abed1e4a3cb42e4b74469919639e04e2997e7
brightchul <57323359+brightchul@users.noreply.github.com>
2022-05-13 11:10:10 +0900

    feat: 디비, 엔터티에 유저 인증 필드 추가

--- src/main/resources/user.sql
+++ src/main/resources/user.sql
@@ -1,12 +1,10 @@
 CREATE TABLE IF NOT EXISTS user (
-    id int NOT NULL AUTO_INCREMENT,
+    id int  AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255),
     nick_name VARCHAR(255) UNIQUE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    is_authentication BOOLEAN DEFAULT FALSE,
     PRIMARY KEY(id),
     INDEX idx1(email)
 );
-
-INSERT INTO user(email, password) VALUES ("asdf@asdf.com", "password");
-INSERT INTO user(email, password) VALUES ("zxcv@asdf.com", "password");
