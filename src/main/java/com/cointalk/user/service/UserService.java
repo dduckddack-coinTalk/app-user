32f907933a82d00b021e12d4fa9b63e7ffdca7ea
brightchul <57323359+brightchul@users.noreply.github.com>
2022-05-13 11:12:00 +0900

    feat: 서비스 계층에서 이메일 인증 관련 기능 추가
    - emailAuthentication는 인증하기 전에 먼저 디비에 관련 유저를 생성
    - 해당 유저가 이메일 인증을 했는지 확인
    - 인증을 안했으면 인증 이메일 발송

--- src/main/java/com/cointalk/user/service/UserService.java
+++ src/main/java/com/cointalk/user/service/UserService.java
@@ -1,16 +1,20 @@
 package com.cointalk.user.service;
 
+import com.cointalk.user.dto.ResponseDto;
 import com.cointalk.user.entity.User;
 import reactor.core.publisher.Mono;
 
 public interface UserService {
     Mono<User> createUser(User user);
 
+
     Mono<User> getUser(String email);
 
     Mono<Integer> updateUser(User user);
 
     Mono<User> login(User user);
+
+    Mono<ResponseDto> emailAuthentication(String authUrl, String email);
 
     Mono<String> userEmailAuthentication(String email);
 }
--- src/main/java/com/cointalk/user/service/UserServiceImpl.java
+++ src/main/java/com/cointalk/user/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cointalk.user.service;
 
+import com.cointalk.user.dto.ResponseDto;
 import com.cointalk.user.entity.User;
 import com.cointalk.user.repository.UserRepository;
 import com.cointalk.user.util.Encryption;
@@ -11,10 +12,13 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final EmailService emailService;
     private final UserRepository userRepository;
 
     public Mono<User> createUser(User user) {
-        user.setPassword(Encryption.encrypt(user.getPassword()));
+        if (user.getPassword() != null) {
+            user.setPassword(Encryption.encrypt(user.getPassword()));
+        }
         return userRepository.save(user);
     }
 
@@ -27,6 +31,21 @@ public class UserServiceImpl implements UserService {
     public Mono<User> login(User user) {
         String password = Encryption.encrypt(user.getPassword());
         return userRepository.findByEmailAndPassword(user.getEmail(), password);
+    }
+
+    @Override
+    public Mono<ResponseDto> emailAuthentication(String authUrl, String email) {
+        return userRepository.findByEmail(email)
+                .switchIfEmpty(userRepository.save(new User(null, email, null, null, null, false)))
+                .filter(User::getIsAuthentication)
+                .map(user -> new ResponseDto("ok", "이미 인증된 이메일"))
+                .switchIfEmpty(Mono.defer(() -> {
+                    if (emailService.sendEmailAuthentication(authUrl, email)) {
+                        return Mono.just(new ResponseDto("ok", "유저 인증 메일 발송 성공"));
+                    } else {
+                        return Mono.just(new ResponseDto("error", "유저 인증 메일 발송 실패"));
+                    }
+                }));
     }
 
     @Override
