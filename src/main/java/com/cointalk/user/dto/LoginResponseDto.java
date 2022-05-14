e154068dfe57a5476dda963b485d5817d44ea4d9
brightchul <57323359+brightchul@users.noreply.github.com>
2022-05-14 21:23:59 +0900

    feat: 로그인 응답 타입 수정 (#12)

--- src/main/java/com/cointalk/user/dto/LoginResponseDto.java
+++ src/main/java/com/cointalk/user/dto/LoginResponseDto.java
@@ -1,5 +1,7 @@
 package com.cointalk.user.dto;
 
+import com.cointalk.user.entity.User;
+import com.cointalk.user.model.UserInfo;
 import lombok.*;
 
 @Getter
@@ -9,10 +11,12 @@ public class LoginResponseDto extends ResponseDto{
 
     private String accessToken;
     private String refreshToken;
+    private UserInfo userInfo;
 
-    public LoginResponseDto(String status, String message, String accessToken, String refreshToken) {
+    public LoginResponseDto(String status, String message, String accessToken, String refreshToken, User user) {
         super(status, message);
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
+        this.userInfo = new UserInfo(user);
     }
 }
