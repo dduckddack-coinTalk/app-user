4280cf6e079f980865aa0c82de772ae490c590b5
brightchul <57323359+brightchul@users.noreply.github.com>
2022-05-15 23:18:29 +0900

    fix: "@EnableWebFlux" 에노테이션 제거
    이것을 제거하는 이유는 해당 에노테이션으로 설정을 다시 해줘야 하는 부분이 생겨서 제거 합니다.

--- src/main/java/com/cointalk/user/config/WebConfig.java
+++ src/main/java/com/cointalk/user/config/WebConfig.java
@@ -1,9 +1,7 @@
 package com.cointalk.user.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @Configuration
-@EnableWebFlux
 public class WebConfig {
 }
