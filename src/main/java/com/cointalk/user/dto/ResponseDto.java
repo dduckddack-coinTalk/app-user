f12c62905f90b42d1df1f6c43e898e3911240f9d
brightchul <57323359+brightchul@users.noreply.github.com>
2022-05-18 09:57:04 +0900

    refactor: 유저 업데이트시 응답 타입 수정  (#18)
    * chore: github action 적용 브랜치 수정
    
    * chore: commitlint에서 알파벳 소문자 허용 추가
    
    * feat: 사용하지 않는 UserInfoDto 제거
    
    * feat: ResponseDto의 static method or, error 를 추가
    
    * feat: 유저 업데이트시 jwt 토큰을 헤더에 추가

--- src/main/java/com/cointalk/user/dto/ResponseDto.java
+++ src/main/java/com/cointalk/user/dto/ResponseDto.java
@@ -7,6 +7,14 @@ public class ResponseDto {
     String status;
     String message;
 
+    static public ResponseDto error(String message) {
+        return new ResponseDto("error", message);
+    }
+
+    static public ResponseDto ok(String message) {
+        return new ResponseDto("ok", message);
+    }
+
     public ResponseDto(String status, String message) {
         this.status = status;
         this.message = message;
